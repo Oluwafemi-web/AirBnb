4a487c5cfe73b28cfeb6965c676954db786cfc06
igeral <ogoayorindeshekinah@gmail.com>
2023-03-14 10:21:41 +0100

    Update LocationItem.js

--- src/components/LocationItem.js
+++ src/components/LocationItem.js
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function LocationItem(props) {
      return (
           <div className="col-lg-4 col-sm-6 col-12">
                <div className="single-location">
                     <div className="location-image">
-                         <a href="#">
+                         <Link to="">
                               <img src={props.image} alt="" />
-                         </a>
+                         </Link>
                     </div>
                     <div className="location-text">
                          <h3>
-                              <a href="#">{props.location}</a>
+                              <Link to="">{props.location}</Link>
                          </h3>
                          <div className="address-distance fix">
                               <span>{props.address}</span>
