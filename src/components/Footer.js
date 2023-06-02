44061afd468fd2919e979514d3511871c2bc489a
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-06-02 20:50:45 +0100

    footer modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,86 +1,90 @@
-import './css/footer.css'
-import Socials from './Socials'
-import { useState, useEffect } from 'react'
-import sanityClient from '../client'
-import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from 'react-icons/fa'
+import "./css/footer.css";
+import Socials from "./Socials";
+import { useState, useEffect } from "react";
+import sanityClient from "../client";
+import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 // import FooterImage from './FooterImage'
 export default function IndexFooter(props) {
-     const [socials, updateSocials] = useState(null)
-     useEffect(() => {
-          sanityClient.fetch(`*[_type == "footer"] {
+  const [socials, updateSocials] = useState(null);
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "footer"] {
                facebook,
                instagram,
-               twitter,
-               pinterest,
+              tiktok,
 
-          }`).then(data => updateSocials(data))
-               .catch(console.error)
-     }, [])
-     return (
-
-          socials && socials.map((item, index) => <footer className="footer-area" key={index}>
-               {/* Footer Widget Start */}
-               <div className="footer-widget-area bg-dark">
-                    <div className="container">
-                         <div className="row mb-n60">
-                              <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-60">
-                                   <div className="single-footer-widget">
-                                        <div className="footer-logo">
-                                             <a href="index.html">
-                                                  <img src={props.logo} alt="Oestin" />
-                                             </a>
-                                        </div>
-                                        <p>
-                                             {props.description}
-                                        </p>
-                                   </div>
-                              </div>
-                              <div className="col-xl-3 offset-lg-1 col-lg-4  col-md-6 col-sm-6 col-12 mb-60">
-                                   <div className="single-footer-widget">
-                                        <h3>contact us</h3>
-                                        <div className="c-info">
-                                             <span>
-                                                  <FaMapMarkerAlt />
-                                             </span>
-                                             <span>
-                                                  {props.address} <br />
-                                             </span>
-                                        </div>
-                                        <div className="c-info">
-                                             <span>
-                                                  <FaEnvelope />
-                                             </span>
-                                             <span>
-                                                  {props.email}
-                                                  <br />
-                                                  {props.email}
-                                             </span>
-                                        </div>
-                                        <div className="c-info">
-                                             <span>
-                                                  <FaPhoneAlt />
-                                             </span>
-                                             <span>
-                                                  {props.number}
-                                                  <br />
-                                                  {props.number}
-                                             </span>
-                                        </div>
-                                   </div>
-                              </div>
-                              <div className='col-xl-3 offset-lg-1 col-lg-3 col-md-6 col-sm-6 col-12 mb-60'>
-                                   <div className='single-footer-widget'>
-                                        <div className="social-icons">
-                                             <Socials facebook={item.facebook} instagram={item.instagram} twitter={item.twitter} pinterest={item.pinterest} />
-                                        </div>
-                                   </div>
-                              </div>
-
-                         </div>
-                    </div>
-               </div>
-
-          </footer>)
-
-     )
+          }`
+      )
+      .then((data) => updateSocials(data))
+      .catch(console.error);
+  }, []);
+  return (
+    socials &&
+    socials.map((item, index) => (
+      <footer className="footer-area" key={index}>
+        {/* Footer Widget Start */}
+        <div className="footer-widget-area bg-dark">
+          <div className="container">
+            <div className="row mb-n60">
+              <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-60">
+                <div className="single-footer-widget">
+                  <div className="footer-logo">
+                    <a href="index.html">
+                      <img src={props.logo} alt="Oestin" />
+                    </a>
+                  </div>
+                  <p>{props.description}</p>
+                </div>
+              </div>
+              <div className="col-xl-3 offset-lg-1 col-lg-4  col-md-6 col-sm-6 col-12 mb-60">
+                <div className="single-footer-widget">
+                  <h3>contact us</h3>
+                  <div className="c-info">
+                    <span>
+                      <FaMapMarkerAlt />
+                    </span>
+                    <span>
+                      {props.address} <br />
+                    </span>
+                  </div>
+                  <div className="c-info">
+                    <span>
+                      <FaEnvelope />
+                    </span>
+                    <span>
+                      {props.email}
+                      <br />
+                      {props.email}
+                    </span>
+                  </div>
+                  <div className="c-info">
+                    <span>
+                      <FaPhoneAlt />
+                    </span>
+                    <span>
+                      {props.number}
+                      <br />
+                      {props.number}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-3 offset-lg-1 col-lg-3 col-md-6 col-sm-6 col-12 mb-60">
+                <div className="single-footer-widget">
+                  <div className="social-icons">
+                    <Socials
+                      facebook={item.facebook}
+                      instagram={item.instagram}
+                      tiktok={item.tiktok}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    ))
+  );
 }
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,24 +1,20 @@
-import { FaFacebookF, FaInstagram, FaPinterestP, FaTwitter } from "react-icons/fa";
-
+import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
 
 export default function Socials(props) {
-     return (
-          <>
-               <a href={props.facebook}>
-                    <FaFacebookF />
-               </a>
-               <a href={props.instagram}>
-                    <FaInstagram />
-               </a>
-               {/* <a href="https://www.rss.com/">
+  return (
+    <>
+      <a href={props.facebook}>
+        <FaFacebookF />
+      </a>
+      <a href={props.instagram}>
+        <FaInstagram />
+      </a>
+      {/* <a href="https://www.rss.com/">
                     <FaRss />
                </a> */}
-               <a href={props.twitter}>
-                    <FaTwitter />
-               </a>
-               <a href={props.pinterest}>
-                    <FaPinterestP />
-               </a>
-          </>
-     )
+      <a href={props.tiktok}>
+        <FaTiktok />
+      </a>
+    </>
+  );
 }
