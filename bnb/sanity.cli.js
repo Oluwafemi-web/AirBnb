dfc3c41327e9cc3d13496724fbb7c7f4811407ff
Ogo-Ayorinde Shekinah <ogoayorindeshekinah@gmail.com>
2023-07-27 17:10:44 +0100

    Update sanity.cli.js

--- bnb/sanity.cli.js
+++ bnb/sanity.cli.js
@@ -1,8 +1,9 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
+  project: {basePath: '/'},
   api: {
     projectId: 'v7ewywok',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
